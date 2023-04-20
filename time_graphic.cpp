58535682996d6d313a224b6c9c1d58d5fe9fb436
camiielle <camillalazzati@gmail.com>
2023-04-20 13:11:46 +0200

    added more data files

---
+++ time_graphic.cpp
@@ -0,0 +1,145 @@
+
+void setStyle() {
+  gROOT->SetStyle("Plain");
+  gStyle->SetPalette(57);
+  gStyle->SetOptTitle(0);
+}
+
+void grafico() {
+
+  // 2kHz graph
+  TMultiGraph *mg = new TMultiGraph();
+  mg->SetTitle("Frequenza - 2kHz; Tempo (ms); Ampiezza (V)");
+
+  TGraph *graph_Gen = new TGraph("Vgen(2kHz)ms.txt", "%lg %lg");
+  mg->Add(graph_Gen);
+
+  TGraph *graph_W = new TGraph("Vw(2kHz)ms.txt", "%lg %lg");
+  mg->Add(graph_W);
+
+  TGraph *graph_T = new TGraph("Vt(2kHz)ms.txt", "%lg %lg");
+  mg->Add(graph_T);
+
+  TGraph *graph_M = new TGraph("Vm(2kHz)ms.txt", "%lg %lg");
+  mg->Add(graph_M);
+
+  // Cosmetics
+  graph_Gen->SetLineColor(kBlue);
+  graph_Gen->SetMarkerColor(kBlue);
+  graph_Gen->SetLineWidth(2);
+
+  graph_W->SetLineColor(kRed);
+  graph_W->SetMarkerColor(kRed);
+  graph_W->SetLineWidth(2);
+
+  graph_T->SetLineColor(kGreen);
+  graph_T->SetMarkerColor(kGreen);
+  graph_T->SetLineWidth(2);
+
+  graph_M->SetLineColor(kMagenta);
+  graph_M->SetMarkerColor(kMagenta);
+  graph_M->SetLineWidth(2);
+
+// Draw the 2kHz multi-graph!
+  TCanvas *myCanvas = new TCanvas();
+  mg->Draw("al");
+  // Build and Draw a legend
+  TLegend *leg = new TLegend(.1, .7, .3, .9, "");
+  leg->AddEntry(graph_Gen, "gen");
+  leg->AddEntry(graph_W, "woofer");
+  leg->AddEntry(graph_T, "tweeter");
+  leg->AddEntry(graph_M, "midrange");
+  leg->Draw("Same");
+
+  // 11kHz graph
+  TMultiGraph *mg2 = new TMultiGraph();
+  mg2->SetTitle("Frequenza - 11kHz; Tempo (ms); Ampiezza (V)");
+
+  TGraph *graph_Gen2 = new TGraph("Vgen(11kHz)ms.txt", "%lg %lg");
+  mg2->Add(graph_Gen2);
+
+  TGraph *graph_W2 = new TGraph("Vw(11kHz)ms.txt", "%lg %lg");
+  mg2->Add(graph_W2);
+
+  TGraph *graph_T2 = new TGraph("Vt(11kHz)ms.txt", "%lg %lg");
+  mg2->Add(graph_T2);
+
+  TGraph *graph_M2 = new TGraph("Vm(11kHz)ms.txt", "%lg %lg");
+  mg2->Add(graph_M2);
+
+  // Cosmetics
+  graph_Gen2->SetLineColor(kBlue);
+  graph_Gen2->SetMarkerColor(kBlue);
+  graph_Gen2->SetLineWidth(2);
+
+  graph_W2->SetLineColor(kRed);
+  graph_W2->SetMarkerColor(kRed);
+  graph_W2->SetLineWidth(2);
+
+  graph_T2->SetLineColor(kGreen);
+  graph_T2->SetMarkerColor(kGreen);
+  graph_T2->SetLineWidth(2);
+
+  graph_M2->SetLineColor(kMagenta);
+  graph_M2->SetMarkerColor(kMagenta);
+  graph_M2->SetLineWidth(2);
+
+  // Draw the 11kHz multi-graph!
+  TCanvas *myCanvas2 = new TCanvas();
+  mg2->Draw("al");
+  // Build and Draw a legend
+  TLegend *leg2 = new TLegend(.1, .7, .3, .9, "");
+  leg2->AddEntry(graph_Gen2, "gen");
+  leg2->AddEntry(graph_W2, "woofer");
+  leg2->AddEntry(graph_T2, "tweeter");
+  leg2->AddEntry(graph_M2, "midrange");
+  leg2->Draw("Same");
+
+  // 5.3kHz graph
+  TMultiGraph *mg3 = new TMultiGraph();
+  mg3->SetTitle("Frequenza - 5.3kHz; Tempo (ms); Ampiezza (V)");
+
+  TGraph *graph_Gen3 = new TGraph("Vgen(5.3kHz)ms - Copia.txt", "%lg %lg");
+  mg3->Add(graph_Gen3);
+
+  TGraph *graph_W3 = new TGraph("Vm(5.3kHz)ms - Copia.txt", "%lg %lg");
+  mg3->Add(graph_W3);
+
+  TGraph *graph_T3 = new TGraph("Vw(5.3kHz)ms - Copia.txt", "%lg %lg");
+  mg3->Add(graph_T3);
+
+  TGraph *graph_M3 = new TGraph("Vt(5.3kHz)ms - Copia.txt", "%lg %lg");
+  mg3->Add(graph_M3);
+
+  // Cosmetics
+  graph_Gen3->SetLineColor(kBlue);
+  graph_Gen3->SetMarkerColor(kBlue);
+  graph_Gen3->SetLineWidth(2);
+
+  graph_W3->SetLineColor(kRed);
+  graph_W3->SetMarkerColor(kRed);
+  graph_W3->SetLineWidth(2);
+
+  graph_T3->SetLineColor(kGreen);
+  graph_T3->SetMarkerColor(kGreen);
+  graph_T3->SetLineWidth(2);
+
+  graph_M3->SetLineColor(kMagenta);
+  graph_M3->SetMarkerColor(kMagenta);
+  graph_M3->SetLineWidth(2);
+
+  TCanvas *myCanvas3 = new TCanvas();
+  // Draw the 5.3kHz multi-graph!
+  mg3->Draw("al");
+  // Build and Draw a legend
+  TLegend *leg3 = new TLegend(.1, .7, .3, .9, "");
+  leg3->AddEntry(graph_Gen3, "gen");
+  leg3->AddEntry(graph_W3, "woofer");
+  leg3->AddEntry(graph_T3, "tweeter");
+  leg3->AddEntry(graph_M3, "midrange");
+  leg3->Draw("Same");
+
+  myCanvas->Print("multigrafico_tempo_2kHz.jpg");
+  myCanvas2->Print("multigrafico_tempo_11kHz.jpg");
+  myCanvas3->Print("multigrafico_tempo_5.3kHz.jpg");
+}
