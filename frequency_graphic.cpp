59bd5b8dbdd4b3cde49e9b9350c2e941841f9a2d
lorenzorizzi17 <102590961+lorenzorizzi17@users.noreply.github.com>
2023-04-22 17:41:17 +0200

    Fit now works properly

--- frequency_graphic.cpp
+++ frequency_graphic.cpp
@@ -1,5 +1,4 @@
 
-
 void setStyle_frequenze() {
   gROOT->SetStyle("Plain");
   gStyle->SetPalette(57);
@@ -46,27 +45,26 @@ void grafico_frequenze() {
   leg->Draw("Same");
 
 
-  //fitting. Let V_0 = 5 V
-  TF1* fitwoofer = new TF1("fitwoofer","5/(sqrt([0]*[0]+(x*x)*([1]*[1])))",2000,11000);
-  fitwoofer->SetParameter(0,1.2);
-  fitwoofer->SetParameter(1,0.000039);
-  fitwoofer->SetParameter(2,5);
+  //fitting. Let V_0 = 4.791 V
+  TF1* fitwoofer = new TF1("fitwoofer","4.791/(sqrt([0]*[0]+4*pi*pi*(x*x)*([1]*[1])))",2000,11000);
+  fitwoofer->SetParameter(0,1.28);
+  fitwoofer->SetParameter(1,0.0000063);
   graph_W->Fit("fitwoofer","R,W,Q");
-  std::cout << "\nFit result for V_W: mu_{w} =" << fitwoofer->GetParameter(0) << " +/- " << fitwoofer->GetParError(0) << ",   tau_{w} = " << fitwoofer->GetParameter(1) << " +/- " << fitwoofer->GetParError(1) << ",    V0 = " << fitwoofer->GetParameter(2) << " +/- " << fitwoofer->GetParError(2) << std::endl;
+  std::cout << "\nFit result for V_W: mu_{w} =" << fitwoofer->GetParameter(0) << " +/- " << fitwoofer->GetParError(0) << ",   tau_{w} = " << fitwoofer->GetParameter(1) << " +/- " << fitwoofer->GetParError(1) << std::endl;
 
-  TF1* fittweeter = new TF1("fittweeter","5/(sqrt([0]*[0]+1/(x*x*[1]*[1])))",2000,11000);
+  TF1* fittweeter = new TF1("fittweeter","4.791/(sqrt([0]*[0]+1/(4*pi*pi*x*x*[1]*[1])))",2000,11000);
   fittweeter->SetParameter(0,-1.2);
-  fittweeter->SetParameter(1,0.000004);
+  fittweeter->SetParameter(1,0.0000063);
   graph_T->Fit("fittweeter","R,W,Q");
   std::cout << "\nFit result for V_T: mu_{t} =" << fittweeter->GetParameter(0) << " +/- " << fittweeter->GetParError(0) << ",  tau_{t} = " << fittweeter->GetParameter(1) << " +/- " << fittweeter->GetParError(1) << std::endl;
   std::cout << '\n';
 
-  TF1* fitmidrange = new TF1("fitmidrange","5/(sqrt( [0]*[0] + ( [1]*x - (1/([2]*x)) )^2 ))",2000,11000);
+  TF1* fitmidrange = new TF1("fitmidrange","4.791/(sqrt( [0]*[0] + ( [1]*x*2*pi - (1/([2]*x*2*pi)) )^2 ))",2000,11000);
   fitmidrange->SetParameter(0,1.28);
-  fitmidrange->SetParameter(1,0.00025);
-  fitmidrange->SetParameter(2,0.0001);
+  fitmidrange->SetParameter(1,0.000063);
+  fitmidrange->SetParameter(2,0.000063);
   graph_M->Fit("fitmidrange","R,W,Q");
-  std::cout << "\nFit result for V_M: mu_{m} =" << fitmidrange->GetParameter(0) << " +/- " << fitmidrange->GetParError(0) << ",  tau_{lm} = " << fitmidrange->GetParameter(1) << " +/- " << fitmidrange->GetParError(1) << ",   tau_{cm} = " << fitmidrange->GetParameter(2) << " +/- " << fitmidrange->GetParError(2) << std::endl;
+  std::cout << "\nFit result for V_M: mu_{m} =" << fitmidrange->GetParameter(0) << " +/- " << fitmidrange->GetParError(0) << ",  tau_{lm} = " << fitmidrange->GetParameter(1) << " +/- " << fitmidrange->GetParError(1) << ",   tau_{cm} = " << fitmidrange->GetParameter(2) << " +/- " << fitmidrange->GetParError(2) << std::endl;;
   std::cout << '\n';
 
   myCanvas->Print("multigrafico_ampiezze_frequenze.jpg");
