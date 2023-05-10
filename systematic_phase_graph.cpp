b68384b386a75cb0d1e758a110ca6e00a657894e
camiielle <camillalazzati@gmail.com>
2023-05-10 10:50:18 +0200

    fixed systematic_phase graph

--- systematic_phase_graph.cpp
+++ systematic_phase_graph.cpp
@@ -6,26 +6,34 @@ void setStyle() {
 }
 
 void grafico_errori() {
-
   TMultiGraph *mg = new TMultiGraph();
-  mg->SetTitle("Fase in funzione della frequenza; Frequenza (Hz); Fase (gradi)");
+  mg->SetTitle(
+      "Fase in funzione della frequenza; Frequenza (Hz); Fase (gradi)");
+
+  TGraph *A0 = new TGraph("sistematico_A0.txt", "%lg %lg");
+  mg->Add(A0);
 
   TGraph *A1 = new TGraph("sistematico_A1.txt", "%lg %lg");
   mg->Add(A1);
 
-  TGraph *A2 = new TGraph("sistematico_A2_offset.txt", "%lg %lg");
+  TGraph *A2 = new TGraph("sistematico_A2.txt", "%lg %lg");
   mg->Add(A2);
 
-  TGraph *A3 = new TGraph("sistematico_A3_offset.txt", "%lg %lg");
+  TGraph *A3 = new TGraph("sistematico_A3.txt", "%lg %lg");
   mg->Add(A3);
 
   // Cosmetics
-  A1->SetLineColor(kOrange-3);
-  A1->SetMarkerColor(kOrange-3);
+
+  A0->SetLineColor(kBlue - 4);
+  A0->SetMarkerColor(kBlue - 4);
+  A0->SetLineWidth(2);
+
+  A1->SetLineColor(kOrange - 3);
+  A1->SetMarkerColor(kOrange - 3);
   A1->SetLineWidth(2);
 
-  A2->SetLineColor(kGreen-9);
-  A2->SetMarkerColor(kGreen-9);
+  A2->SetLineColor(kGreen - 9);
+  A2->SetMarkerColor(kGreen - 9);
   A2->SetLineWidth(2);
 
   A3->SetLineColor(kMagenta);
@@ -33,10 +41,12 @@ void grafico_errori() {
   A3->SetLineWidth(2);
 
   // Draw the multi-graph!
-  TCanvas *myCanvas = new TCanvas("myCanvas","myCanvas", 800,380);
+  TCanvas *myCanvas = new TCanvas("myCanvas", "myCanvas", 800, 380);
   mg->Draw("al");
   // Build and Draw a legend
-  TLegend *leg = new TLegend(0.1290727,0.8230337,0.2092732,0.8848315,NULL,"brNDC");
+  TLegend *leg =
+      new TLegend(0.1290727, 0.8230337, 0.2092732, 0.8848315, NULL, "brNDC");
+  leg->AddEntry(A0, "AI_0");
   leg->AddEntry(A1, "AI_1");
   leg->AddEntry(A2, "AI_2");
   leg->AddEntry(A3, "AI_3");
@@ -47,33 +57,48 @@ void grafico_errori() {
   myCanvas->Print("multigrafico_sistematico_fase.pdf");
   myCanvas->Print("multigrafico_sistematico_fase.tex");
 
+  // perofrming a linear fit
 
-  //perofrming a linear fit 
-    TF1 *f_A1 = new TF1("f_A1","([0]+[1]*x)",2050,11000);
-    f_A1->SetParameter(0,0.);
-    A1->Fit("f_A1","R,Q");
-    std::cout << "\nFit result for A1: offset_{A1} =" << f_A1->GetParameter(0) << " +/- " << f_A1->GetParError(0) << ",  slope_{A1} = " 
-    << f_A1->GetParameter(1) << " +/- " << f_A1->GetParError(1) << std::endl;
-    std::cout << '\n';
+  TF1 *f_A0 = new TF1("f_A0", "([0]+[1]*x)", 2050, 11000);
+  f_A0->SetParameter(0, 0.);
+  A1->Fit("f_A0", "R,Q, N");
+  std::cout << "\nFit result for A0: offset_{A0} =" << f_A0->GetParameter(0)
+            << " +/- " << f_A0->GetParError(0)
+            << ",  slope_{A1} = " << f_A0->GetParameter(1) << " +/- "
+            << f_A0->GetParError(1) << std::endl;
+  std::cout << '\n';
+  TF1 *f_A1 = new TF1("f_A1", "([0]+[1]*x)", 2050, 11000);
+  f_A1->SetParameter(0, 0.);
+  A1->Fit("f_A1", "R,Q,N");
+  std::cout << "\nFit result for A1: offset_{A1} =" << f_A1->GetParameter(0)
+            << " +/- " << f_A1->GetParError(0)
+            << ",  slope_{A1} = " << f_A1->GetParameter(1) << " +/- "
+            << f_A1->GetParError(1) << std::endl;
+  std::cout << '\n';
 
-    TF1 *f_A2 = new TF1("f_A2","([0]+[1]*x)",2000,11000);
-    f_A2->SetParameter(0,1.);
-    A2->Fit("f_A2","R,Q");
-    std::cout << "\nFit result for A2: offset_{A2} =" << f_A2->GetParameter(0) << " +/- " << f_A2->GetParError(0) << ",  slope_{A2} = " 
-    << f_A2->GetParameter(1) << " +/- " << f_A2->GetParError(1) << std::endl;
-    std::cout << '\n';
-    
-    TF1 *f_A3 = new TF1("f_A3","([0]+[1]*x)",2050,11000);
-    f_A3->SetParameter(0,2.);
-    A3->Fit("f_A3","R,Q");
-    std::cout << "\nFit result for A3: offset_{A3} =" << f_A3->GetParameter(0) << " +/- " << f_A3->GetParError(0) << ",  slope_{A3} = " 
-    << f_A3->GetParameter(1) << " +/- " << f_A3->GetParError(1) << std::endl;
-    std::cout << '\n';
+  TF1 *f_A2 = new TF1("f_A2", "([0]+[1]*x)", 2000, 11000);
+  f_A2->SetParameter(0, 1.);
+  A2->Fit("f_A2", "R,Q,N");
+  std::cout << "\nFit result for A2: offset_{A2} =" << f_A2->GetParameter(0)
+            << " +/- " << f_A2->GetParError(0)
+            << ",  slope_{A2} = " << f_A2->GetParameter(1) << " +/- "
+            << f_A2->GetParError(1) << std::endl;
+  std::cout << '\n';
+
+  TF1 *f_A3 = new TF1("f_A3", "([0]+[1]*x)", 2050, 11000);
+  f_A3->SetParameter(0, 2.);
+  A3->Fit("f_A3", "R,Q,N");
+  std::cout << "\nFit result for A3: offset_{A3} =" << f_A3->GetParameter(0)
+            << " +/- " << f_A3->GetParError(0)
+            << ",  slope_{A3} = " << f_A3->GetParameter(1) << " +/- "
+            << f_A3->GetParError(1) << std::endl;
+  std::cout << '\n';
 }
 
-void grafico_fasi(){
-TMultiGraph *mg = new TMultiGraph();
-  mg->SetTitle("Fase in funzione della frequenza; Frequenza (Hz); Fase (gradi)");
+void grafico_fasi() {
+  TMultiGraph *mg = new TMultiGraph();
+  mg->SetTitle(
+      "Fase in funzione della frequenza; Frequenza (Hz); Fase (gradi)");
 
   TGraph *fase_w = new TGraph("Fase_w.txt", "%lg %lg");
   mg->Add(fase_w);
@@ -85,12 +110,12 @@ TMultiGraph *mg = new TMultiGraph();
   mg->Add(fase_m);
 
   // Cosmetics
-  fase_w->SetLineColor(kOrange-3);
-  fase_w->SetMarkerColor(kOrange-3);
+  fase_w->SetLineColor(kOrange - 3);
+  fase_w->SetMarkerColor(kOrange - 3);
   fase_w->SetLineWidth(2);
 
-  fase_t->SetLineColor(kGreen-9);
-  fase_t->SetMarkerColor(kGreen-9);
+  fase_t->SetLineColor(kGreen - 9);
+  fase_t->SetMarkerColor(kGreen - 9);
   fase_t->SetLineWidth(2);
 
   fase_m->SetLineColor(kMagenta);
@@ -98,10 +123,11 @@ TMultiGraph *mg = new TMultiGraph();
   fase_m->SetLineWidth(2);
 
   // Draw the multi-graph!
-  TCanvas *myCanvas = new TCanvas("myCanvas","myCanvas", 900,500);
+  TCanvas *myCanvas = new TCanvas("myCanvas", "myCanvas", 900, 500);
   mg->Draw("al");
   // Build and Draw a legend
-  TLegend *leg = new TLegend(0.7862372,0.8115055,0.8850659,0.8812729,NULL,"brNDC");
+  TLegend *leg =
+      new TLegend(0.7862372, 0.8115055, 0.8850659, 0.8812729, NULL, "brNDC");
   leg->AddEntry(fase_w, "woofer");
   leg->AddEntry(fase_t, "tweeter");
   leg->AddEntry(fase_m, "midrange");
@@ -111,5 +137,4 @@ TMultiGraph *mg = new TMultiGraph();
   myCanvas->Print("multigrafico_fase.jpg");
   myCanvas->Print("multigrafico_fase.pdf");
   myCanvas->Print("multigrafico_fase.tex");
-
 }
