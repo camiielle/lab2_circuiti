874f171c86e4a67a569fb86cf6627a3fbf98f167
camiielle <camillalazzati@gmail.com>
2023-05-10 18:39:09 +0200

    removed W option from fitting, divided data by 5

--- frequency_graphic_senzaFGEN.cpp
+++ frequency_graphic_senzaFGEN.cpp
@@ -10,13 +10,13 @@ void grafico_frequenze() {
   TMultiGraph *mg = new TMultiGraph();
   mg->SetTitle("Ampiezza in funzione della frequenza; Frequenza (Hz); Ampiezza (V)");
 
-  TGraphErrors *graph_W = new TGraphErrors("Ampiezza_w.txt", "%lg %lg %lg");
+  TGraphErrors *graph_W = new TGraphErrors("Ampiezza_w_div5.txt", "%lg %lg %lg");
   mg->Add(graph_W);
 
-  TGraphErrors *graph_T = new TGraphErrors("Ampiezza_t.txt", "%lg %lg %lg");
+  TGraphErrors *graph_T = new TGraphErrors("Ampiezza_t_div5.txt", "%lg %lg %lg");
   mg->Add(graph_T);
 
-  TGraphErrors *graph_M = new TGraphErrors("Ampiezza_m.txt", "%lg %lg");
+  TGraphErrors *graph_M = new TGraphErrors("Ampiezza_m_div5.txt", "%lg %lg %lg");
   mg->Add(graph_M);
 
   // Cosmetics
@@ -45,25 +45,25 @@ void grafico_frequenze() {
   leg->Draw("Same");
 
 
-  //fitting. Let V_0 = 4.791 V
-  TF1* fitwoofer = new TF1("fitwoofer","4.791/(sqrt([0]*[0]+4*pi*pi*(x*x)*([1]*[1])))",2000,11000);
+  //fitting gain function, i.e. Voltage/5V
+  TF1* fitwoofer = new TF1("fitwoofer","1./(sqrt([0]*[0]+4*pi*pi*(x*x)*([1]*[1])))",2000,11000);
   fitwoofer->SetParameter(0,1.28);
   fitwoofer->SetParameter(1,0.0000063);
-  graph_W->Fit("fitwoofer","R,W,Q");
+  graph_W->Fit("fitwoofer","R,Q");
   std::cout << "\nFit result for V_W: mu_{w} =" << fitwoofer->GetParameter(0) << " +/- " << fitwoofer->GetParError(0) << ",   tau_{w} = " << fitwoofer->GetParameter(1) << " +/- " << fitwoofer->GetParError(1) << std::endl;
 
-  TF1* fittweeter = new TF1("fittweeter","4.791/(sqrt([0]*[0]+1/(4*pi*pi*x*x*[1]*[1])))",2000,11000);
+  TF1* fittweeter = new TF1("fittweeter","1./(sqrt([0]*[0]+1/(4*pi*pi*x*x*[1]*[1])))",2000,11000);
   fittweeter->SetParameter(0,-1.2);
   fittweeter->SetParameter(1,0.0000063);
-  graph_T->Fit("fittweeter","R,W,Q");
+  graph_T->Fit("fittweeter","R,Q");
   std::cout << "\nFit result for V_T: mu_{t} =" << fittweeter->GetParameter(0) << " +/- " << fittweeter->GetParError(0) << ",  tau_{t} = " << fittweeter->GetParameter(1) << " +/- " << fittweeter->GetParError(1) << std::endl;
   std::cout << '\n';
 
-  TF1* fitmidrange = new TF1("fitmidrange","4.791/(sqrt( [0]*[0] + ( [1]*x*2*pi - (1/([2]*x*2*pi)) )^2 ))",2000,11000);
+  TF1* fitmidrange = new TF1("fitmidrange","1./(sqrt( [0]*[0] + ( [1]*x*2*pi - (1/([2]*x*2*pi)) )^2 ))",2000,11000);
   fitmidrange->SetParameter(0,1.28);
   fitmidrange->SetParameter(1,0.000063);
   fitmidrange->SetParameter(2,0.000063);
-  graph_M->Fit("fitmidrange","R,W,Q");
+  graph_M->Fit("fitmidrange","R,Q");
   std::cout << "\nFit result for V_M: mu_{m} =" << fitmidrange->GetParameter(0) << " +/- " << fitmidrange->GetParError(0) << ",  tau_{lm} = " << fitmidrange->GetParameter(1) << " +/- " << fitmidrange->GetParError(1) << ",   tau_{cm} = " << fitmidrange->GetParameter(2) << " +/- " << fitmidrange->GetParError(2) << std::endl;;
   std::cout << '\n';
 
